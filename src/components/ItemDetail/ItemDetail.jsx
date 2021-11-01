d5f0494c00fe2eec9b6a09528011598914f0658e
Facundo Rodriguez Orge <facundo.rodriguez.orge@gmail.com>
2021-11-01 15:00:41 -0300

    ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,6 +5,7 @@ export default function ItemDetail(item) {
     console.log(item.item)
     return (
         <div className='item-detail__card'>
+            <h1>ITEM DETAIL</h1>
             <img src={item.item.url} alt="" className='item-detail__image'/>
             <span className='item-detail__name'>{item.item.name}</span>
             <span className='item-detail__price'>{item.item.price}</span>
