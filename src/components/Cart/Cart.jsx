b557c79538f06e758e623316f31e198dcaf56aab
Facundo Rodriguez Orge <facundo.rodriguez.orge@gmail.com>
2021-11-15 15:03:42 -0300

    EmptyCart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react/cjs/react.development";
 import { CartContext } from "../../context/CartContext";
+import EmptyCart from "../EmptyCart/EmptyCart";
 
 export default function Cart() {
   const { cartData, clear, removeItem, total } = useContext(CartContext);
@@ -10,7 +11,7 @@ export default function Cart() {
     <div>
       <button onClick={() => clear()}>Vaciar Carrito</button>
       <ul>
-        {cartData ? cartData.map(item => <><li> {item.name} ${item.price} Cantidad {item.quantity} </li><button onClick={() =>removeItem(item.id)}>X</button></>) : 'NADA'}
+        {cartData ? cartData.map(item => <><li> {item.name} ${item.price} Cantidad {item.quantity} </li><button onClick={() =>removeItem(item.id)}>X</button></>) : <EmptyCart />}
         <li>Total: ${total}</li>
         </ul>
         
---
+++ src/components/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div>
+            <h1>EL CARRITO ESTÁ VACÍO</h1>
+        </div>
+    )
+}
