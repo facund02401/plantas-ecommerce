4b2e8a427de3c5769d6c74689501af42298fc877
Facundo Rodriguez Orge <facundo.rodriguez.orge@gmail.com>
2021-11-17 19:58:16 -0300

    itemDetail funcionando con firebase

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -6,23 +6,23 @@ import ItemCounter from "../ItemCount/ItemCount";
 
 export default function ItemDetail(item) {
   const [cartQty, setCartQty] = useState(0);
-  const { addItem, isInCart, cartData, total} = useContext(CartContext)
+  const { addItem, isInCart, cartData, total } = useContext(CartContext);
+  const itemInUse = item.item[0];
 
   const saveQty = (counterQty) => {
     setCartQty(counterQty);
     //setTotal(+item.item.price * item.item.quantity)
     addItem(item.item, counterQty);
-    
   };
-console.log(cartData)
-console.log(total)
-  
+  console.log(cartData);
+  console.log(total);
+
   return (
     <div className="item-detail__card">
-      <img src={item.item.url} alt="" className="item-detail__image" />
-      <span className="item-detail__name">{item.item.name}</span>
-      <span className="item-detail__price"> ${item.item.price}</span>
-      <span className="item-detail__stock">Stock: {item.item.stock}</span>
+      <img src={itemInUse.url} alt="" className="item-detail__image" />
+      <span className="item-detail__name">{itemInUse.name}</span>
+      <span className="item-detail__price"> ${itemInUse.price}</span>
+      <span className="item-detail__stock">Stock: {itemInUse.stock}</span>
       <p className="item-detail__description">
         <b>Descripcion: </b> {item.item.description}
       </p>
--- src/components/ItemDetailContainter/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainter/ItemDetailContainer.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import Products from "../productos.json";
+//import Products from "../productos.json";
+import { getFirestore } from "../../firebase";
+import { collection, getDocs, docs, setDoc, query, where } from "firebase/firestore";
 
 export default function ItemDetailContainer() {
   const [selectedItem, setSelectedItem] = useState("");
   const {id} = useParams()
   const itemId = parseInt(id)
   
-  const getItem = (item) =>
+
+useEffect(()=>{
+  const db = getFirestore();
+
+  const q = query(collection(db, "item"), where("id", "==", itemId));
+  getDocs(q).then(snap => setSelectedItem(snap.docs.map(doc => doc.data())))
+
+
+},[])
+
+
+  /* const getItem = (item) =>
     new Promise((resolve, reject) => {
       setTimeout(() => {
         if (item) {
@@ -26,7 +39,7 @@ export default function ItemDetailContainer() {
         setSelectedItem(filtrado);
       })
       .catch((err) => console.log(err));
-  }, [itemId]);
+  }, [itemId]); */
 
   return (
     <div>{selectedItem ? <ItemDetail item={selectedItem} /> : "Cargando"}</div>
