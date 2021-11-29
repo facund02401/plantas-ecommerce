37e836324f93dab2b3e04475df8d15246af4ba7d
Facundo Rodriguez Orge <facundo.rodriguez.orge@gmail.com>
2021-11-29 18:14:21 -0300

    Arreglo itemListContainer

--- src/components/ItemListContainer/ItemListContainter.jsx
+++ src/components/ItemListContainer/ItemListContainter.jsx
@@ -27,24 +27,7 @@ function ItemListContainer() {
 
   }, [id]);
 
-  //  const getItems = (items /*Funcion que crea una promesa*/) =>
-  //    new Promise((resolve, reject) => {
-  //      setTimeout(() => {
-  //        if (items) {
-  //          resolve(items);
-  //        } else {
-  //          reject("Hubo un error al acceder a los productos.");
-  //        }
-  //      }, 2000);
-  //    });
-  //  useEffect(() => {
-  //    getItems(Products)
-  //      .then((res) => {id?
-  //        setProductos(res.filter((product) => product.category === id)): setProductos(Products); //Filtra los productos cuyo id coincida con el parametro de la url (establecido en NavBar)
-  //      }) //Si se resuelve, guarda el array como 'productos'
-  //      .catch((err) => console.log(err));
-  //  }, [id]);
-  //
+  
   return (
     <section>
       <ItemList items={productos} />
